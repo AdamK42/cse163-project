808ef9876414eb501113c52adf5d735a5f41963e
perezkay <perezkay@uw.edu>
2020-03-04 19:10:31 -0800

    fixed some style errors

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,10 +1,29 @@
-import pandas as pd
-
-def read_in_dataset(file_name):
+def get_school_names(df):
     """
-    Takes a dataset name. Returns the dataset as a pandas dataframe
+    Takes a dataframe. Returns a list of the schools in the dataframe.
     """
-    return pd.read_excel(file_name)
+    col_names = list(df.columns)
+    schools = list()
+    for name in col_names[1:]:
+        # so we don't even need regexes???
+        # name = re.sub(r"#+", "", name) # removes tag
+        words = name.split()  # splits into individual words
+        school_list = words[3:len(words) - 2]  # gets a list of the school name
+        school = " ".join(school_list)  # gets a string of the school name
+        schools.append(school)
+    return schools
 
-df = read_in_dataset("datasets/total_applicants.xlsx")
-df
+
+def get_school_data(df, school_names):
+    """
+    Takes a school data dataframe and a list of school names. Returns a
+    dictionary where the keys are school names and the values are lists of
+    tuples (year, data number)
+    """
+    admitted_data = dict()
+    for i in range(1, len(school_names)):
+        numbers = df.loc[:, df.columns[i]]
+        years = df.loc[:, "Time"]
+        school_data = list(zip(years, numbers))
+        admitted_data[school_names[i]] = school_data
+    return admitted_data
