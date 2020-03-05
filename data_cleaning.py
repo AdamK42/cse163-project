dac03577f43cb2504b6efe640853171692f82ebe
Kayla <kayla.perez270@gmail.com>
2020-03-04 19:38:54 -0800

    Left some junk comments in, removed them and cleaned up a bit

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,15 +1,13 @@
 def get_school_names(df):
     """
-    Takes a dataframe. Returns a list of the schools in the dataframe.
+    Takes a school data DataFrame. Returns a list of the schools in the DataFrame.
     """
     col_names = list(df.columns)
     schools = list()
     for name in col_names[1:]:
-        # so we don't even need regexes???
-        # name = re.sub(r"#+", "", name) # removes tag
-        words = name.split()  # splits into individual words
-        school_list = words[3:len(words) - 2]  # gets a list of the school name
-        school = " ".join(school_list)  # gets a string of the school name
+        words = name.split()
+        school_list = words[3:len(words) - 2]
+        school = " ".join(school_list)
         schools.append(school)
     return schools
 
@@ -20,10 +18,10 @@ def get_school_data(df, school_names):
     dictionary where the keys are school names and the values are lists of
     tuples (year, data number)
     """
-    admitted_data = dict()
+    statistic_data = dict()
     for i in range(1, len(school_names)):
         numbers = df.loc[:, df.columns[i]]
         years = df.loc[:, "Time"]
         school_data = list(zip(years, numbers))
-        admitted_data[school_names[i]] = school_data
-    return admitted_data
+        statistic_data[school_names[i]] = school_data
+    return statistic_data
