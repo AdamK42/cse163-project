9a7b7e179f794bb2708cbf728ba1418fce40c511
Adam Klingler <aoklingler@gmail.com>
2020-03-12 15:48:13 -0700

    Dataframe construction code

--- creating_dataframe.py
+++ creating_dataframe.py
@@ -71,3 +71,30 @@ def is_public_or_private(df, all_names, public_names, private_names):
             df.loc["private", name] = 1
         else:
             df.loc["private", name] = 0
+
+def main():
+    grad_rate_dict = get_school_data(grad_rates_df, grad_rate_names, "grad_rate")
+    pop_dict = get_school_data(population_df, pop_names, "population")
+    applicant_dict = get_school_data(applicants_df, applicants_names, "applicants")
+    admitted_dict = get_school_data(admitted_df, admitted_names, "admitted")
+    private_dict = get_school_data(fin_aid_private_df, private_names, "fin_aid")
+    public_dict = get_school_data(fin_aid_public_df, public_names, "fin_aid")
+
+    grad_df = pd.DataFrame(grad_rate_dict)
+    pop_df = pd.DataFrame(pop_dict)
+    applicant_df = pd.DataFrame(applicant_dict)
+    admitted_df = pd.DataFrame(admitted_dict)
+    private_df = pd.DataFrame(private_dict)
+    public_df = pd.DataFrame(public_dict)
+
+    df = applicant_df.copy()
+    df = df.merge(grad_df, how="outer", on=["School", "Year"])
+    df = df.merge(pop_df, how="outer", on=["School", "Year"])
+    df = df.merge(admitted_df, how="outer", on=["School", "Year"])
+    df = df.merge(private_df, how="outer", on=["School", "Year"])
+    df = df.merge(public_df, how="outer", on=["School", "Year", "fin_aid"])
+
+    df.replace(0, np.nan, inplace=True)
+
+if __name__ == "__main__":
+    main()
